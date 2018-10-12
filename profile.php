5a2d188dd19195a59eb0bbf9237c39e77db39dbd
Joe-Pena <joepena@protonmail.com>
2018-10-11 20:25:11 -0600

    push3

---
+++ profile.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title')
+Cuffing Army
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-6 m-3 mr-5">
+        <img src="http://cdn01.cdn.justjared.com/wp-content/uploads/2008/04/hulk-shirtless/hulk-hogan-shirtless-15.jpg" class="pic mil-guy">
+    <div class="p-6 ml-6 col">
+            <h3 class="white">Mr. Hulk Hogan</h3>
+            <span class="white fact">Age: 64</span><br>
+            <span class="white fact">Hobbies: Drinking</span><br>
+            <span class="white fact">Location: Wherever you are!</span><br>
+            <span class="white fact">Dislikes:</span><br>
+            <span class="white fact">Hobbies:</span><br>
+            <span class="white fact">Location: Wherever you are!</span>
+        </div>
+    </div>
+</div>
+
+@endsection
