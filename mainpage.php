f4c25c09ff14059137b88950d0b2936bbdc651b0
Joe-Pena <joepena@protonmail.com>
2018-10-11 20:04:18 -0600

    blah

---
+++ mainpage.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title')
+Cuffing Army
+@endsection
+
+@section('content')
+<div class="cuffing-desc mt-4 mb-5 p-4">
+        <h2>What is cuffing?</h2>
+        <p>During the Fall and Winter months people who would normally rather be single or promiscuous find themselves along with the rest of the world desiring to be "Cuffed" or tied down by a serious relationship. The cold weather and prolonged indoor activity causes singles to become lonely and desperate to be cuffed.</p>
+    </div>
+
+    <div class="row separator">
+    </div>
+
+    <div class="p-4 row">
+    <img class="greenbg mil-guy col-3" src="https://c1.staticflickr.com/1/9/68444109_b31b7af181.jpg">
+    <div class="col-8">
+    <h2 class="white">2018's MVP</h2>
+    <h3 class="white">Mr. Chuck Norris</h3>
+    <span class="white fact">Age: 64</span><br>
+    <span class="white fact">Hobbies: Drinking</span>
+    <span class="white fact">Location: Wherever you are!</span>
+    <span class="white fact">Dislikes:</span><br>
+    <span class="white fact">Hobbies:</span>
+    <span class="white fact">Location: Wherever you are!</span>
+    </div>
+
+</div>
+@endsection
