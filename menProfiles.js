15b93b3ba5821a00d4a9c2af80bc115d385a4af3
Shannon Callahan <ShannonC619@gmail.com>
2018-10-11 19:18:23 -0600

    inital commit for profiles

---
+++ menProfiles.js
@@ -0,0 +1,37 @@
+const profiles = [
+  { name: "John Smith",
+    age: 30,
+    location: "Washington, DC",
+    like: "Dogs",
+    dislike: "Cats",
+    hobby: "Knitting"
+  },
+  { name: "Richard Smith",
+    age: 23,
+    location: "San Diego, CA",
+    like: "Surfing",
+    dislike: "Hiking",
+    hobby: "Drinking coffee"
+  },
+  { name: "Tim Smith",
+    age: 27,
+    location: "Denver, CO",
+    like: "Hiking",
+    dislike: "Cooking",
+    hobby: "Trying new foods/beers"
+  },
+  { name: "Butch Smith",
+    age: 32,
+    location: "Boston, MA",
+    like: "fishing",
+    dislike: "Cold",
+    hobby: "Watching Boston Red Sox playing"
+  },
+  { name: "Kevin Smith",
+    age: 25,
+    location: "Miami, FL",
+    like: "Tanning",
+    dislike: "Marijuana",
+    hobby: "Get some sun at beach"
+  }
+]
