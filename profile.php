afa6850d12c3ba81e54fbc3c64311c6779ec5c1c
Joe-Pena <joepena@protonmail.com>
2018-10-11 20:33:19 -0600

    pushy5

--- profile.php
+++ profile.php
@@ -7,17 +7,17 @@ Cuffing Army
 @section('content')
 <div class="row">
     <div class="col-6 m-3 mr-5">
-        <img src="http://cdn01.cdn.justjared.com/wp-content/uploads/2008/04/hulk-shirtless/hulk-hogan-shirtless-15.jpg" class="pic mil-guy">
+        <img src="{{ $guy->picture}}" class="pic mil-guy">
     <div class="p-6 ml-6 col">
-            <h3 class="white">Mr. Hulk Hogan</h3>
-            <span class="white fact">Age: 64</span><br>
-            <span class="white fact">Hobbies: Drinking</span><br>
-            <span class="white fact">Location: Wherever you are!</span><br>
-            <span class="white fact">Dislikes:</span><br>
-            <span class="white fact">Hobbies:</span><br>
-            <span class="white fact">Location: Wherever you are!</span>
+            <h3 class="white">{{ $guy->name }}</h3>
+            <span class="white fact">{{ $guy->age }}</span><br>
+            <span class="white fact">{{ $guy->hobbies}}</span><br>
+            <span class="white fact">{{ $guy->location}}</span><br>
+            <span class="white fact">{{ $guy->likes}}</span><br>
+            <span class="white fact">{{ $guy->dislikes}}</span><br>
+            <span class="white fact">{{ $guy->rating}}</span><br>
         </div>
     </div>
-</div>
+    </div>
 
 @endsection
